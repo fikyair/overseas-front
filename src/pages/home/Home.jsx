e7d1ec2a0d00694bc3ff8721d0d57dec79b07a30
fikyair <shiming_xsm@163.com>
2019-05-09 18:02:23 +0800

    加入highCharts

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-export const PAGE_ROUTE = '/home';
+export const PAGE_ROUTE = '/home'
 export default class Home extends Component {
   render() {
-    return (
-      <div>
-                hi,this is my home
-      </div>
-    );
+    return <div>hi,this is my home</div>
   }
 }
