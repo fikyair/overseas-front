3a370246750a8a6af5c6368e5894c691117b4ee0
fikyair <shiming_xsm@163.com>
2019-05-24 19:14:32 +0800

    :cyclone:页面布局优化

--- src/pages/intl/Intl.jsx
+++ src/pages/intl/Intl.jsx
@@ -1,15 +1,63 @@
 import React, { Component } from 'react';
 import styles from './style.less';
+import tabledemo3 from './image/item-bg.png';
+import WordOutside from './image/word-outside.png';
+import WordInside from './image/word-inside.png';
 
 export const PAGE_ROUTE = '/intl';
 export default class Home extends Component {
-  render() {
-    return (
-      <div className={styles.indexWrap}>
-        {
-          console.log('==>', styles)
-        }
-      </div>
-    );
-  }
+    state = {
+      imageData: [
+        tabledemo3, tabledemo3, tabledemo3,
+      ],
+    }
+
+    render() {
+      return (
+        <div className={styles.indexWrap}>
+          <div className={styles.navTop}>
+             YOYO
+          </div>
+          <div className={styles.contentWrap}>
+            <div className={styles.side}>
+              <div className={styles.sideTitle}>
+                 左
+              </div>
+              {
+                this.state.imageData.map(item => (
+                  <div className={styles.sideContent}>
+                    <img src={item} alt="图片" className={styles.img} />
+                  </div>
+                ))
+              }
+            </div>
+            <div className={styles.middleWrap}>
+              <div className={styles.middle}>
+                <div className={styles.middleTitle}>
+                        中
+                </div>
+                <div className={styles.outside}>
+                  <img src={WordOutside} alt="" width="100%" />
+                </div>
+                <div className={styles.inside}>
+                  <img src={WordInside} alt="" width="100%" />
+                </div>
+                <img src={tabledemo3} alt="图片" className={styles.middleRank} />
+              </div>
+              <div className={styles.middleBottom}>
+                        三个
+              </div>
+            </div>
+            <div className={styles.side}>
+              <div className={styles.sideTitle}>
+                 右
+              </div>
+              <div className={styles.sideContent}>
+                表格数据
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
 }
