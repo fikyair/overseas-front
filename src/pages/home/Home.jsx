4003fe6e3478a24cd7e5b4f655de375d13bc49be
fikyair <shiming_xsm@163.com>
2019-11-22 10:48:54 +0800

    :zap:feature: support number precise

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import * as precision from '../../utils/number-precision';
 import PageContent from '../../layouts/page-content';
 
 export const PAGE_ROUTE = '/home'
 export default class Home extends Component {
-  render() {
-    return (
-      <PageContent>
-        hi,this is my home
-      </PageContent>
-    )
-  }
+    render() {
+        return (
+            <PageContent>
+                hi,this is my home
+                {
+                    console.log('divide', precision.plus(0.1, 0.2))
+                }
+            </PageContent>
+        )
+    }
 }
